270cd3de14219136aa553095e4b827a5e3d28f4d
Adivigo Khalishtama <khalishtama25@gmail.com>
2025-01-16 13:01:45 +0700

    feat: argon di create user

---
+++ routes/topup.route.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ppay/controllers"
+)
+
+func TopupRoute(router *gin.Engine) {
+	topupGroup := router.Group("/topup")
+	{
+		topupGroup.POST("", controllers.CreateUser)
+	}
+}
