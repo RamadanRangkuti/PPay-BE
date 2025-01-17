641526c0ebf7978cabb468850e3c70796e85d11c
rama <ramadanrangkuti17@gmail.com>
2025-01-17 15:25:57 +0700

    fixing update and hashing pin

--- internal/dto/auth.dto.go
+++ internal/dto/auth.dto.go
@@ -9,3 +9,7 @@ type LoginDTO struct {
 	Email    string `json:"email" form:"email" binding:"required,email"`
 	Password string `json:"password" form:"password" binding:"required,min=6"`
 }
+
+type PinDTO struct {
+	Pin string `json:"pin" form:"pin"`
+}
